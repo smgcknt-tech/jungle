7ba3a7e042db9ed1bc92cedc86034f37d0a69a0e
konnichiwa sekai <54715182+smgcknt-tech@users.noreply.github.com>
2021-04-09 18:07:43 +0900

    added app.use(express.urlencoded({extended: false}));

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-//controllersからimportするroutes処理--------------------------------------
+//controllersからimportするrouter------------------------------------------
 import TopRouter from './routes/TopRouter.js'
-
 //------------------------------------------------------------------------
 const app = express();
 dotenv.config();
 app.use(cors());
 app.use(express.static('public'));
-
+app.use(express.urlencoded({extended: false}));
 //router ※corsの下に配置--------------------------------------------------
 app.use('/',TopRouter)
 //------------------------------------------------------------------------
@@ -20,5 +19,4 @@ const PORT = process.env.PORT|| 3000;
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log('Server Running on Port: http://localhost:${PORT}')))
   .catch((error) => console.log(`${error} did not connect`));
-
 mongoose.set('useFindAndModify', false);
