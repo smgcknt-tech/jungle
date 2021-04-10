352f4059d5971203c7fb20c54746f75d975e207a
smgcknt-tech <business.smgcknt@gmail.com>
2021-04-10 15:58:13 +0900

    installed jquery and changed index.js

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 //controllersからimportするroutes処理--------------------------------------
 import TopRouter from './routes/TopRouter.js'
 
 //------------------------------------------------------------------------
 const app = express();
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 dotenv.config();
 app.use(cors());
-app.use(express.static('public'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
 //router ※corsの下に配置--------------------------------------------------
-app.use('/',TopRouter)
+app.use('/', TopRouter)
 //------------------------------------------------------------------------
 
-const PORT = process.env.PORT|| 3000;
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log('Server Running on Port: http://localhost:${PORT}')))
