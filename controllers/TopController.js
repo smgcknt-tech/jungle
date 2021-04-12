43c28e260d408e55acff47e6e3e1a68393ec3c0b
konnichiwa sekai <54715182+smgcknt-tech@users.noreply.github.com>
2021-04-12 18:57:38 +0900

    updated top

--- controllers/TopController.js
+++ controllers/TopController.js
@@ -1,8 +1,9 @@
 import listInputs from "../models/ProductData.js";
+import data from "../models/data.js";
 
 export const getTop = (req, res) => {
   try {
-    res.render(`Top.ejs`);
+    res.render(`Top.ejs`,{products:data.products});
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
