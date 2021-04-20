2a343165d5a21b0e64d5d774e89ee4d9d4a16e55
konnichiwa sekai <54715182+smgcknt-tech@users.noreply.github.com>
2021-04-20 18:58:49 +0900

    stucked hard

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -1,8 +1,20 @@
-$(function(){
 
-  $('#cartNum').change(()=> {
-    let url = $('#cartForm').attr('action') +'?qty='+ $('#cartNum').val();
-    $('#cartForm').attr('action',url);
-  } );
+$(function () {
+  //cartCard.ejs//
+  $("#cartNum").change(() => {
+    let url = $("#cartForm").attr("action") + "?qty=" + $("#cartNum").val();
+    $("#cartForm").attr("action", url);
+  });
+  //cart.ejs//
+
+
+  let sumItem = 0;
+    $(".item_qty").each(() => {
+      let val = Number($('.item_qty').val());
+      sumItem += val
+    });
+  $("#sum_qty").text("合計：" + sumItem + "点");
+  
 
 });
+
