926b2d49e11ac4fd71d2dd7e19771eba8bcef808
smgcknt-tech <business.smgcknt@gmail.com>
2021-04-22 23:01:36 +0900

    finished cart calculation

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -5,36 +5,58 @@ $(function () {
     $("#cartForm").attr("action", url);
   });
   //cart.ejs//
-  
-  
+
+
   let totalItem = 0;
   $(".eachInput").each((i, val) => {
-    let a  = Number($(val).val());
+    let a = Number($(val).val());
     totalItem += a;
   });
   $("#sum_qty").text("合計：" + totalItem + "点");
-  
 
 
 
-  $('.eachInput').change(()=>{
-  let totalItem = 0
-  $('.eachInput').each((i,val) => {
-    let a = Number($(val).val());
-    totalItem = totalItem + a;
-    console.log(totalItem)
-    $("#sum_qty").text("合計：" + totalItem + "点");
-  });
-});
+
+  $('.eachInput').change(() => {
+    let eachPrice = []
+    $('.eachPrice').each((i, val) => {
+      a = Number($(val).text());
+      eachPrice.push(a)
+    });
+    let totalItem = 0;
+    let sumPrice = []
+    $('.eachInput').each((i, val) => {
+      let a = Number($(val).val());
+      totalItem += a;
+      let b = a * eachPrice[i];
+      sumPrice.push(b)
+      $("#sum_qty").text("合計：" + totalItem + "点");
+    });
+
+    console.log(sumPrice)
+    $('.sumPrice').each((i, val) => {
+      $(val).text(sumPrice[i])
+    })
 
 
-  
-  $(".sumPrice").each((i, val) => {
+
     let totalPrice = 0;
+    $(".sumPrice").each((i, val) => {
+      let b = Number($(val).text());
+      totalPrice += b;
+      $("#sum_price").text("合計：" + totalPrice + "円");
+    });
+
+  });
+
+
+
+  let totalPrice = 0;
+  $(".sumPrice").each((i, val) => {
     let b = Number($(val).text());
     totalPrice += b;
-    $("#sum_price").text("合計：" + totalPrice + "円");  
+    $("#sum_price").text("合計：" + totalPrice + "円");
   });
 
-  
+
 });
