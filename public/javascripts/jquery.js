30e69c4d94a90dcf0d8cbb73c82960cf8a154649
konnichiwa sekai <54715182+smgcknt-tech@users.noreply.github.com>
2021-04-22 18:04:58 +0900

    changed cart.ejs and jquery

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -5,18 +5,36 @@ $(function () {
     $("#cartForm").attr("action", url);
   });
   //cart.ejs//
+  
+  
   let totalItem = 0;
-  $(".eachSelect-1 option").each((i, val) => {
-    let a = Number($(val).val());
+  $(".eachInput").each((i, val) => {
+    let a  = Number($(val).val());
     totalItem += a;
   });
+  $("#sum_qty").text("合計：" + totalItem + "点");
+  
 
-  let totalPrice = 0;
+
+
+  $('.eachInput').change(()=>{
+  let totalItem = 0
+  $('.eachInput').each((i,val) => {
+    let a = Number($(val).val());
+    totalItem = totalItem + a;
+    console.log(totalItem)
+    $("#sum_qty").text("合計：" + totalItem + "点");
+  });
+});
+
+
+  
   $(".sumPrice").each((i, val) => {
-    b = Number($(val).text());
+    let totalPrice = 0;
+    let b = Number($(val).text());
     totalPrice += b;
+    $("#sum_price").text("合計：" + totalPrice + "円");  
   });
 
-  $("#sum_price").text("合計：" + totalPrice + "円");
-  $("#sum_qty").text("合計：" + totalItem + "点");
+  
 });
