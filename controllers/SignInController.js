38181936f014719fcfc426b07019b02f01e7aa3f
konnichiwa sekai <54715182+smgcknt-tech@users.noreply.github.com>
2021-04-26 15:31:08 +0900

    implemented signin

--- controllers/SignInController.js
+++ controllers/SignInController.js
@@ -1,9 +1,26 @@
-const data =require ("../models/schemas/Data.js");
+const { users } = require("../models/schemas/Data.js");
+const User = require("../models/schemas/UserModels.js");
 
-const SignInController ={
-    getSignIn:(req, res) => {
-        res.render("SignIn.ejs");
-    }
-}
+const SignInController = {
+  getSignIn: (req, res) => {
+    res.render("SignIn.ejs");
+  },
+  postSignIn: (req, res) => {
+    //ユーザー認証
+    const email = req.body.email;
+    const password = req.body.password;
+    User.find({ email: email }, (err, results) => {
+      if (results.length > 0) {
+        if (password === results[0].password) {
+          console.log("認証成功");
+          res.redirect("/");
+        } else {
+          console.log("認証失敗");
+          res.redirect("/signIn");
+        }
+      }
+    });
+  },
+};
 
-module.exports =  SignInController;
+module.exports = SignInController;
--- routes/SignInRouter.js
+++ routes/SignInRouter.js
@@ -2,5 +2,5 @@ const express =require ("express");
 const SignInController = require("../controllers/SignInController");
 const router = express.Router();
 router.get('/',SignInController.getSignIn,);
-
+router.post('/',SignInController.postSignIn,)
 module.exports =  router
