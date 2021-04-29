e44060ebcebe7057714007f69f2aae3331b34ac7
konnichiwa sekai <54715182+smgcknt-tech@users.noreply.github.com>
2021-04-29 14:41:55 +0900

    c

--- controllers/TopController.js
+++ controllers/TopController.js
@@ -7,12 +7,9 @@ module.exports = {
     res.render("Top.ejs", {products:allProducts});
     }
 }
-process.on('unhandledRejection', (reason, p) => {
-    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
-})
 
 //error-catcher
 process.on("unhandledRejection", (reason, p) => {
     console.log("Unhandled Rejection at: Promise", p, "reason:", reason);
-  });
+});
   
