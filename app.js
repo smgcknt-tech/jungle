cb7d9c5cb6e0b9690a53d30422d25478334d0366
konnichiwa sekai <54715182+smgcknt-tech@users.noreply.github.com>
2021-05-20 13:55:33 +0900

    deploy heroku

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })); 
 app.use(session({ secret: "my_secret_key", resave: false, saveUninitialized: false }));
-app.use(express.static(path.join('public')));
+app.use(express.static(path.join(__dirname,'public')));
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use([
   MidllewareController.loginCheck, 
