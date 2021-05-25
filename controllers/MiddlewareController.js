f74e169b162e448b09d81bccf49d5626fd860a25
konnichiwa sekai <54715182+smgcknt-tech@users.noreply.github.com>
2021-05-25 17:45:28 +0900

    fixed admin

--- controllers/MiddlewareController.js
+++ controllers/MiddlewareController.js
@@ -1,34 +1,30 @@
 const Products = require("../models/Products.js");
-const p = Products
+const p = Products;
 const Users = require("../models/Users");
 const u = Users;
 
 module.exports = {
-  loginCheck:async (req, res, next) => {
-    res.locals.categories = await p.product.findCategory()
-    res.locals.brands = await p.product.findBrand()
-    res.locals.isAdmin = req.session.isAdmin
-    if(req.session.userId === undefined){
-      res.locals.isLoggedIn = false
-      res.locals.userName = 'ゲスト'
+  loginCheck: async (req, res, next) => {
+    res.locals.categories = await p.product.findCategory();
+    res.locals.brands = await p.product.findBrand();
+    res.locals.isAdmin = req.session.isAdmin;
+    if (req.session.userId === undefined) {
+      res.locals.isLoggedIn = false;
+      res.locals.userName = "ゲスト";
       res.locals.cartItem = [];
-    } else　{
-      const userId = req.session.userId
-      const userData = await u.midlleware.findUserdata(userId)
-      res.locals.userData  = userData
-      res.locals.cartItem  = await p.cart.findAll(userId);
-      res.locals.shipping  = await u.checkOut.findShipping(userId);
-      res.locals.isLoggedIn = true
-      res.locals.userName = req.session.userName
-
+    } else {
+      const userId = req.session.userId;
+      const userData = await u.midlleware.findUserdata(userId);
+      res.locals.userData = userData;
+      res.locals.cartItem = await p.cart.findAll(userId);
+      res.locals.shipping = await u.checkOut.findShipping(userId);
+      res.locals.isLoggedIn = true;
+      res.locals.userName = req.session.userName;
     }
     next();
   },
-  errorCatcher:(err, req, res, next) => {
+  errorCatcher: (err, req, res, next) => {
     res.status(500).send({ message: err.message });
     next();
-  }
-}
-
-
-
+  },
+};
